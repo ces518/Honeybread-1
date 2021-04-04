798c6abb91d9f5b3e8b2c07538f9d14abc58a8ad
ncucu <ncucu.me@kakaocommerce.com>
2021-04-05 00:30:11 +0900

    refactor : 컨트롤러 테스트코드 리팩토링

--- honeybread-mgmtadmin/src/test/java/com/whatsub/honeybread/mgmtadmin/domain/category/CategoryControllerTest.java
+++ honeybread-mgmtadmin/src/test/java/com/whatsub/honeybread/mgmtadmin/domain/category/CategoryControllerTest.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
@@ -49,6 +50,9 @@ class CategoryControllerTest {
     @MockBean
     CategoryQueryService queryService;
 
+    @Mock
+    CategoryResponse response;
+
     @Test
     void 검색_요청시_검색조건이_없다면_SIZE_만큼_조회에_성공한다() throws Exception {
         // given
@@ -61,20 +65,13 @@ class CategoryControllerTest {
             .willReturn(new PageImpl<>(mockCategories, pageRequest, mockCategories.size()));
 
         // when
-        ResultActions result = mockMvc.perform(
-            get(BASE_URL)
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_검색();
 
         // then
-        verify(queryService).getCategories(any(Pageable.class), any(CategorySearch.class));
+        카테고리_검색이_수행되어야_한다();
 
-        result.andExpect(status().isOk())
-            .andExpect(jsonPath("$.content").isNotEmpty())
-            .andExpect(jsonPath("$.content.length()").value(size))
-            .andExpect(jsonPath("$.content[0].id").exists())
-            .andExpect(jsonPath("$.content[0].name").exists());
+        Ok응답_확인(result);
+        검색_응답_필드_확인(size, result);
     }
 
     @Test
@@ -92,110 +89,68 @@ class CategoryControllerTest {
             .willReturn(new PageImpl<>(willSearchCategories, pageRequest, willSearchCategories.size()));
 
         // when
-        ResultActions result = mockMvc.perform(
-            get(BASE_URL)
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_검색();
 
         // then
-        verify(queryService).getCategories(any(Pageable.class), any(CategorySearch.class));
+        카테고리_검색이_수행되어야_한다();
 
-        result.andExpect(status().isOk())
-            .andExpect(jsonPath("$.content").isNotEmpty())
-            .andExpect(jsonPath("$.content.length()").value(willSearchCategories.size()))
-            .andExpect(jsonPath("$.content[0].id").exists())
-            .andExpect(jsonPath("$.content[0].name").value(search.getName()));
+        Ok응답_확인(result);
+        검색_응답_필드_확인(willSearchCategories.size(), result);
     }
 
     @Test
     void 카테고리가_존재한다면_조회에_성공한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-
-        CategoryResponse 카테고리_응답 = CategoryResponse.builder()
-            .id(categoryId)
-            .name("한식")
-            .build();
-
-        given(queryService.getCategory(anyLong())).willReturn(카테고리_응답);
+        카테고리_조회시_성공한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            get(BASE_URL + "/" + categoryId)
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_조회();
 
         // then
-        verify(queryService).getCategory(anyLong());
+        카테고리_조회가_수행되어야_한다();
 
-        result.andExpect(status().isOk())
-            .andExpect(jsonPath("$.id").value(카테고리_응답.getId()))
-            .andExpect(jsonPath("$.name").value(카테고리_응답.getName()));
+        Ok응답_확인(result);
+        result.andExpect(jsonPath("$.id").value(response.getId()))
+            .andExpect(jsonPath("$.name").value(response.getName()));
     }
 
     @Test
     void 카테고리가_존재하지_않는다면_조회에_실패한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-
-        given(queryService.getCategory(anyLong())).willThrow(new HoneyBreadException(ErrorCode.CATEGORY_NOT_FOUND));
+        카테고리_조회시_실패한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            get(BASE_URL + "/" + categoryId)
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_조회();
 
         // then
-        verify(queryService).getCategory(anyLong());
-
-        result.andExpect(status().isNotFound());
+        카테고리_조회가_수행되어야_한다();
+        NotFound_응답_확인(result);
     }
 
     @Test
     void 중복되는_카테고리가_없다면_등록에_성공한다() throws Exception {
         // given
-        CategoryRequest 카테고리_생성_요청 = generateRequest("한식");
-
-        given(service.create(any(CategoryRequest.class))).willReturn(Long.MIN_VALUE);
+        카테고리_등록시_성공한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            post(BASE_URL)
-                .content(mapper.writeValueAsString(카테고리_생성_요청))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_등록();
 
         // then
-        verify(service).create(any(CategoryRequest.class));
-
-        result.andExpect(status().isCreated());
+        카테고리_등록이_수행되어야_한다();
+        Created_응답_확인(result);
     }
 
     @Test
     void 중복되는_카테고리가_있다면_등록에_실패한다() throws Exception {
         // given
-        CategoryRequest 카테고리_생성_요청 = generateRequest("한식");
-
-        given(service.create(any(CategoryRequest.class)))
-            .willThrow(new HoneyBreadException(ErrorCode.DUPLICATE_CATEGORY));
+        카테고리_등록시_중복_카테고리가_존재한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            post(BASE_URL)
-                .content(mapper.writeValueAsString(카테고리_생성_요청))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_등록();
 
         // then
-        verify(service).create(any(CategoryRequest.class));
-
-        result.andExpect(status().is4xxClientError());
+        카테고리_등록이_수행되어야_한다();
+        BadRequest_응답_확인(result);
     }
 
     @ParameterizedTest
@@ -204,19 +159,16 @@ class CategoryControllerTest {
         "한식중식양식일식abcdefghijklmnopqrstuvwxyz123456789010가나다라마바사아자차카"
     })
     void 카테고리명이_2글자_미만_50자초과라면_등록에_실패한다(String name) throws Exception {
-        // given
-        CategoryRequest 카테고리_생성_요청 = generateRequest(name);
-
         // when
         ResultActions result = mockMvc.perform(
             post(BASE_URL)
-                .content(mapper.writeValueAsString(카테고리_생성_요청))
+                .content(mapper.writeValueAsString(카테고리_요청(name)))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
 
         // then
-        result.andExpect(status().is4xxClientError());
+        BadRequest_응답_확인(result);
     }
 
     @ParameterizedTest
@@ -228,67 +180,45 @@ class CategoryControllerTest {
     })
     void 카테고리명이_2글자_이상_50자_이하라면_등록에_성공한다(String name) throws Exception {
         // given
-        CategoryRequest 카테고리_생성_요청 = generateRequest(name);
-
-        given(service.create(any(CategoryRequest.class))).willReturn(Long.MIN_VALUE);
+        카테고리_등록시_성공한다();
 
         // when
         ResultActions result = mockMvc.perform(
             post(BASE_URL)
-                .content(mapper.writeValueAsString(카테고리_생성_요청))
+                .content(mapper.writeValueAsString(카테고리_요청(name)))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
 
         // then
-        verify(service).create(any(CategoryRequest.class));
-
-        result.andExpect(status().isCreated());
+        카테고리_등록이_수행되어야_한다();
+        Created_응답_확인(result);
     }
 
     @Test
     void 중복되는_카테고리가_없다면_수정에_성공한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-        CategoryRequest 카테고리_수정_요청 = generateRequest("한식을 중식으로 수정");
-
-        willDoNothing().given(service).update(anyLong(), any(CategoryRequest.class));
+        카테고리_수정시_성공한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            put(BASE_URL + "/" + categoryId)
-                .content(mapper.writeValueAsString(카테고리_수정_요청))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_수정();
 
         // then
-        verify(service).update(anyLong(), any(CategoryRequest.class));
-
-        result.andExpect(status().isOk());
+        카테고리_수정이_수행되어야_한다();
+        Ok응답_확인(result);
     }
 
     @Test
     void 중복되는_카테고리가_있다면_수정에_실패한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-        CategoryRequest 카테고리_수정_요청 = generateRequest("한식을 중식으로 수정");
-
-        willThrow(new HoneyBreadException(ErrorCode.DUPLICATE_CATEGORY))
-            .given(service).update(anyLong(), any(CategoryRequest.class));
+        카테고리_수정시_중복_카테고리가_존재한다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            put(BASE_URL + "/" + categoryId)
-                .content(mapper.writeValueAsString(카테고리_수정_요청))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_수정();
 
         // then
-        verify(service).update(anyLong(), any(CategoryRequest.class));
-
-        result.andExpect(status().isConflict());
+        카테고리_수정이_수행되어야_한다();
+        Conflict_응답_확인(result);
     }
 
     @ParameterizedTest
@@ -297,20 +227,16 @@ class CategoryControllerTest {
         "한식중식양식일식abcdefghijklmnopqrstuvwxyz123456789010가나다라마바사아자차카"
     })
     void 카테고리명이_2글자_미만_50자_초과라면_수정에_실패한다(String name) throws Exception {
-        // given
-        final Long categoryId = 1L;
-        CategoryRequest 카테고리_수정_요청 = generateRequest(name);
-
         // when
         ResultActions result = mockMvc.perform(
-            put(BASE_URL + "/" + categoryId)
-                .content(mapper.writeValueAsString(카테고리_수정_요청))
+            put(BASE_URL + "/1")
+                .content(mapper.writeValueAsString(카테고리_요청(name)))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
 
         // then
-        result.andExpect(status().is4xxClientError());
+        BadRequest_응답_확인(result);
     }
 
     @ParameterizedTest
@@ -322,89 +248,201 @@ class CategoryControllerTest {
     })
     void 카테고리명이_2글자_이상_50자_이하라면_수정에_성공한다(String name) throws Exception {
         // given
-        final Long categoryId = 1L;
-        CategoryRequest 카테고리_수정_요청 = generateRequest(name);
-
-        willDoNothing().given(service).update(anyLong(), any(CategoryRequest.class));
+        카테고리_수정시_성공한다();
 
         // when
         ResultActions result = mockMvc.perform(
-            put(BASE_URL + "/" + categoryId)
-                .content(mapper.writeValueAsString(카테고리_수정_요청))
+            put(BASE_URL + "/1")
+                .content(mapper.writeValueAsString(카테고리_요청(name)))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
 
         // then
-        verify(service).update(anyLong(), any(CategoryRequest.class));
-
-        result.andExpect(status().isOk());
+        카테고리_수정이_수행되어야_한다();
+        Ok응답_확인(result);
     }
 
     @Test
     void 카테고리가_존재하지_않는다면_수정에_실패한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-        CategoryRequest 카테고리_수정_요청 = generateRequest("한식에서 중식으로 수정");
-
-        willThrow(new HoneyBreadException(ErrorCode.CATEGORY_NOT_FOUND))
-            .given(service).update(anyLong(), any(CategoryRequest.class));
+        카테고리_수정시_카테고리가_존재하지_않는다();
 
         // when
-        ResultActions result = mockMvc.perform(
-            put(BASE_URL + "/" + categoryId)
-                .content(mapper.writeValueAsString(카테고리_수정_요청))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
-
+        ResultActions result = 카테고리_수정();
         // then
-        verify(service).update(anyLong(), any(CategoryRequest.class));
-
-        result.andExpect(status().isNotFound());
+        카테고리_수정이_수행되어야_한다();
+        NotFound_응답_확인(result);
     }
 
     @Test
     void 카테고리가_존재할경우_삭제에_성공한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-
         willDoNothing().given(service).delete(anyLong());
 
         // when
-        ResultActions result = mockMvc.perform(
-            delete(BASE_URL + "/" + categoryId)
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ResultActions result = 카테고리_삭제();
 
         // then
-        verify(service).delete(anyLong());
-
-        result.andExpect(status().isNoContent());
+        카테고리_삭제가_수행되어야_한다();
+        NoContent_응답_확인(result);
     }
 
     @Test
     void 카테고리가_존재하지_않을경우_삭제에_실패한다() throws Exception {
         // given
-        final Long categoryId = 1L;
-
         willThrow(new HoneyBreadException(ErrorCode.CATEGORY_NOT_FOUND)).given(service).delete(anyLong());
 
         // when
-        ResultActions result = mockMvc.perform(
-            delete(BASE_URL + "/" + categoryId)
+        ResultActions result = 카테고리_삭제();
+
+        // then
+        카테고리_삭제가_수행되어야_한다();
+        NotFound_응답_확인(result);
+    }
+
+    /**
+     * Given
+     */
+    private void 카테고리_조회시_성공한다() {
+        given(response.getId()).willReturn(1L);
+        given(response.getName()).willReturn("한식");
+        given(queryService.getCategory(anyLong())).willReturn(response);
+    }
+
+    private void 카테고리_조회시_실패한다() {
+        given(queryService.getCategory(anyLong())).willThrow(new HoneyBreadException(ErrorCode.CATEGORY_NOT_FOUND));
+    }
+
+    private void 카테고리_등록시_성공한다() {
+        given(service.create(any(CategoryRequest.class))).willReturn(Long.MIN_VALUE);
+    }
+
+    private void 카테고리_등록시_중복_카테고리가_존재한다() {
+        given(service.create(any(CategoryRequest.class)))
+            .willThrow(new HoneyBreadException(ErrorCode.DUPLICATE_CATEGORY));
+    }
+
+    private void 카테고리_수정시_성공한다() {
+        willDoNothing().given(service).update(anyLong(), any(CategoryRequest.class));
+    }
+
+    private void 카테고리_수정시_중복_카테고리가_존재한다() {
+        willThrow(new HoneyBreadException(ErrorCode.DUPLICATE_CATEGORY))
+            .given(service).update(anyLong(), any(CategoryRequest.class));
+    }
+
+    private void 카테고리_수정시_카테고리가_존재하지_않는다() {
+        willThrow(new HoneyBreadException(ErrorCode.CATEGORY_NOT_FOUND))
+            .given(service).update(anyLong(), any(CategoryRequest.class));
+    }
+
+    /**
+     * When
+     */
+    private ResultActions 카테고리_검색() throws Exception {
+        return mockMvc.perform(
+            get(BASE_URL)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
+    }
 
-        // then
-        verify(service).delete(anyLong());
+    private ResultActions 카테고리_조회() throws Exception {
+        ResultActions result = mockMvc.perform(
+            get(BASE_URL + "/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+        return result;
+    }
 
+    private ResultActions 카테고리_등록() throws Exception {
+        return mockMvc.perform(
+            post(BASE_URL)
+                .content(mapper.writeValueAsString(카테고리_요청("한식")))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+    }
+
+    private ResultActions 카테고리_수정() throws Exception {
+        return mockMvc.perform(
+            put(BASE_URL + "/1")
+                .content(mapper.writeValueAsString(카테고리_요청("한식을 중식으로 수정")))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+    }
+
+    private ResultActions 카테고리_삭제() throws Exception {
+        return mockMvc.perform(
+            delete(BASE_URL + "/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+    }
+
+
+    /**
+     * Then
+     */
+    private void 카테고리_검색이_수행되어야_한다() {
+        then(queryService).should().getCategories(any(Pageable.class), any(CategorySearch.class));
+    }
+
+    private void 카테고리_조회가_수행되어야_한다() {
+        then(queryService).should().getCategory(anyLong());
+    }
+
+    private void 카테고리_등록이_수행되어야_한다() {
+        then(service).should().create(any(CategoryRequest.class));
+    }
+
+    private void 카테고리_수정이_수행되어야_한다() {
+        then(service).should().update(anyLong(), any(CategoryRequest.class));
+    }
+
+    private void 카테고리_삭제가_수행되어야_한다() {
+        then(service).should().delete(anyLong());
+    }
+
+    private void 검색_응답_필드_확인(int size, ResultActions result) throws Exception {
+        result.andExpect(jsonPath("$.content").isNotEmpty())
+            .andExpect(jsonPath("$.content.length()").value(size))
+            .andExpect(jsonPath("$.content[0].id").exists())
+            .andExpect(jsonPath("$.content[0].name").exists());
+    }
+
+    private void Ok응답_확인(ResultActions result) throws Exception {
+        result.andExpect(status().isOk());
+    }
+
+    private void Created_응답_확인(ResultActions result) throws Exception {
+        result.andExpect(status().isCreated());
+    }
+
+    private void NoContent_응답_확인(ResultActions result) throws Exception {
+        result.andExpect(status().isNoContent());
+    }
+
+    private void BadRequest_응답_확인(ResultActions result) throws Exception {
+        result.andExpect(status().is4xxClientError());
+    }
+
+    private void NotFound_응답_확인(ResultActions result) throws Exception {
         result.andExpect(status().isNotFound());
     }
 
-    private CategoryRequest generateRequest(String name) {
+    private void Conflict_응답_확인(ResultActions result) throws Exception {
+        result.andExpect(status().isConflict());
+    }
+
+
+    /**
+     * Helper
+     */
+    private CategoryRequest 카테고리_요청(String name) {
         return new CategoryRequest(name);
     }
 
